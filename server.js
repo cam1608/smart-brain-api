687b1c9b6863ee4bcb7bf15300bbe9a16f2a9cd0
Caraiman Mario <caraimanmario@Laptop-Caraiman.local>
2024-06-20 11:41:23 +0300

    adding port

--- server.js
+++ server.js
@@ -36,6 +36,6 @@ app.put('/image', (req, res) => { handleImage(req, res, db) });
 app.post('/imageurl', async (req, res) => { handleImageUrl(req, res) })
 
 
-app.listen(3000, () => {
-    console.log("app is working on port 3000");
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`app is working on port ${process.env.PORT}`);
 });
