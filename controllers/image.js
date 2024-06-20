2faaa585d3a74ed4f831332a2e4385d5d0b10f15
Caraiman Mario <caraimanmario@Laptop-Caraiman.local>
2024-06-20 11:05:54 +0300

    final project

---
+++ controllers/image.js
@@ -0,0 +1,57 @@
+
+const handleImage = (req, res, db) => {
+    const { id } = req.body;
+    db('users')
+    .where("id", "=", id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+        res.json(entries[0].entries);
+    })
+    .catch(err => res.status(400).json('unable to get entries'));
+}
+
+const handleImageUrl = async(req, res) => {
+    const { input } = req.body;
+    const PAT = '865d77de05694305ac4d46a4c40fc93a';
+    const USER_ID = 'clarifai';
+    const APP_ID = 'main';
+    const MODEL_ID = 'face-detection';
+    const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';
+
+    const raw = JSON.stringify({
+    "user_app_id": {
+        "user_id": USER_ID,
+        "app_id": APP_ID,
+    },
+    "inputs": [
+        {
+        "data": {
+            "image": {
+            "url": input,
+            },
+        },
+        },
+    ],
+    });
+
+    const requestOptions = {
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'Authorization': 'Key ' + PAT,
+        'Content-Type': 'application/json'
+    },
+    body: raw
+    };
+
+    try {
+    const response = await fetch(`https://api.clarifai.com/v2/models/${MODEL_ID}/versions/${MODEL_VERSION_ID}/outputs`, requestOptions);
+    const data = await response.json();
+    res.json(data);
+    } catch (error) {
+    res.status(400).json('unable to work with API');
+    }
+    };
+
+export { handleImage, handleImageUrl };
---
+++ server.js
@@ -0,0 +1,41 @@
+import express from "express";
+import bodyParser from "body-parser";
+import bcrypt from "bcrypt-nodejs";
+import cors from "cors";
+import knex from 'knex';
+
+import handleRegister from "./controllers/register.js";
+import handleSignin from "./controllers/signin.js";
+import handleProfileGet from "./controllers/profile.js";
+import { handleImage, handleImageUrl } from "./controllers/image.js";
+
+const db = knex({
+    client: 'pg',
+    connection: {
+      host: '127.0.0.1',
+      port: 5432,
+      user: 'caraimanmario',
+      password: '',
+      database: 'smart-brain',
+    },
+});
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send('server is working');
+});
+
+app.post('/signin', (req, res) => { handleSignin(req, res, db, bcrypt) });
+app.post('/register', (req, res) => { handleRegister(req, res, db, bcrypt) });
+app.get('/profile/:id', (req, res) => { handleProfileGet(req, res, db) });
+app.put('/image', (req, res) => { handleImage(req, res, db) });
+app.post('/imageurl', async (req, res) => { handleImageUrl(req, res) })
+
+
+app.listen(3000, () => {
+    console.log("app is working on port 3000");
+});
